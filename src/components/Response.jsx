1d3e54faa178f8ca144074e51226607e8a3840cc
RileyGoodXYZ <lihengliu787@gmail.com>
2026-06-13 11:09:52 -0700

    File structure

---
+++ src/components/Response.jsx
@@ -0,0 +1,8 @@
+export default function Response(){
+    return (
+        <div className="response">
+            <h2>Response</h2>
+            <p>This is a simple response component.</p>
+        </div>
+    );
+}
